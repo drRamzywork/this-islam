489155bcafb33cf356bd795aeae6d085e656809a
moelshenawy <mohamedgk5@gmail.com>
2024-08-22 20:05:57 +0300

    /

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 import "@/styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Cairo } from "next/font/google";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
+
 const cairo = Cairo({
   subsets: ["latin"],
   weight: ["200", "300", "400", "600", "700", "900", "1000"],
@@ -9,8 +12,15 @@ const cairo = Cairo({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={` ${cairo.className}`}>
-      <Component {...pageProps} />
-    </main>
+    <SimpleBar
+      style={{
+        maxHeight: "100vh",
+        overflowX: "hidden",
+      }}
+    >
+      <main className={` ${cairo.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </SimpleBar>
   );
 }
